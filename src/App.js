aac51b7b1ee94b2851181489d1febe282d71cb48
dionelo <dionelmartin@gmail.com>
2021-09-28 17:31:30 -0300

    Share code on github

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import data from './data';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="grid-container">
+      <header className="row">
+        <div>
+          <a className="brand" href="/">Mercadito</a>
+        </div>
+        <div>
+          <a href="/cart">Changuito</a>
+          <a href="/signin">Ingresar</a>
+        </div>
       </header>
+      <main>
+        <div>
+          <div className="row center">
+
+            {
+              data.products.map(product => (
+                <div key={product._id} className="card">
+                  <a href={`/product/${product._id}`}>
+                    <img className="medium" src={product.image} alt={product.image}/>
+                  </a>
+                  <div className="card-body">
+                    <a href={`/product/${product._id}`}>
+                      <h2>{product.name}</h2>
+                    </a>
+                    <div className="rating">
+                      <span><i className="fas fa-star"></i></span>
+                      <span><i className="fas fa-star"></i></span>
+                      <span><i className="fas fa-star"></i></span>
+                      <span><i className="fas fa-star"></i></span>
+                      <span><i className="far fa-star"></i></span>
+                    </div>
+                    <div className="price">
+                      ${product.price}
+                    </div>
+                  </div>
+                </div>
+              ))
+            }
+                
+          </div>
+        </div>
+      </main>
+      <footer className="row center">
+        Todos los derechos reservados
+      </footer>
     </div>
   );
 }
