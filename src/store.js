19c6f3ab30b9db383df99406a71830560cc112da
dionelo <dionelmartin@gmail.com>
2021-10-06 14:04:23 -0300

    Implement add to cart

--- src/store.js
+++ src/store.js
@@ -1,12 +1,23 @@
 import { applyMiddleware, combineReducers, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import { productDetailsReducer, productListReducer } from './reducers/productReducers';
+import {
+	productDetailsReducer,
+	productListReducer,
+} from './reducers/productReducers';
+import { cartReducer } from './reducers/cartReducers';
 
-const initialState = {};
+const initialState = {
+	cart: {
+		cartItems: localStorage.getItem('cartItems')
+			? JSON.parse(localStorage.getItem('cartItems'))
+			: [],
+	},
+};
 const reducer = combineReducers({
 	productList: productListReducer,
-	productDetails: productDetailsReducer
+	productDetails: productDetailsReducer,
+	cart: cartReducer,
 });
 const store = createStore(
 	reducer,
